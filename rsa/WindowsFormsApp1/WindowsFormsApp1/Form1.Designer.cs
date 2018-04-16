c277c7569fd385578ac6dcd662dc2305495108e4
choina <rafal.dziubak97@gmail.com>
2018-04-17 00:40:57 +0200

    v3

---
+++ rsa/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblKeySizeValue = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mniRSA = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtPublicKey = new System.Windows.Forms.TextBox();
+            this.txtPrivateKey = new System.Windows.Forms.TextBox();
+            this.mniKeySize = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniKeySize512 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniKeySize1024 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key Size:";
+            // 
+            // lblKeySizeValue
+            // 
+            this.lblKeySizeValue.AutoSize = true;
+            this.lblKeySizeValue.Location = new System.Drawing.Point(510, 37);
+            this.lblKeySizeValue.Name = "lblKeySizeValue";
+            this.lblKeySizeValue.Size = new System.Drawing.Size(0, 13);
+            this.lblKeySizeValue.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Public key:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(501, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Private key:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(822, 27);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 8;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(903, 27);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 9;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniRSA});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mniRSA
+            // 
+            this.mniRSA.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniKeySize});
+            this.mniRSA.Name = "mniRSA";
+            this.mniRSA.Size = new System.Drawing.Size(50, 20);
+            this.mniRSA.Text = "Menu";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 56);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(480, 400);
+            this.txtContent.TabIndex = 12;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(504, 56);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(480, 400);
+            this.txtResult.TabIndex = 13;
+            // 
+            // txtPublicKey
+            // 
+            this.txtPublicKey.Location = new System.Drawing.Point(12, 475);
+            this.txtPublicKey.Multiline = true;
+            this.txtPublicKey.Name = "txtPublicKey";
+            this.txtPublicKey.Size = new System.Drawing.Size(480, 100);
+            this.txtPublicKey.TabIndex = 14;
+            // 
+            // txtPrivateKey
+            // 
+            this.txtPrivateKey.Location = new System.Drawing.Point(504, 475);
+            this.txtPrivateKey.Multiline = true;
+            this.txtPrivateKey.Name = "txtPrivateKey";
+            this.txtPrivateKey.Size = new System.Drawing.Size(480, 100);
+            this.txtPrivateKey.TabIndex = 15;
+            // 
+            // mniKeySize
+            // 
+            this.mniKeySize.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniKeySize512,
+            this.mniKeySize1024});
+            this.mniKeySize.Name = "mniKeySize";
+            this.mniKeySize.Size = new System.Drawing.Size(152, 22);
+            this.mniKeySize.Text = "KeySize";
+
+            // 
+            // mniKeySize512
+            // 
+            this.mniKeySize512.Checked = true;
+            this.mniKeySize512.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mniKeySize512.Name = "mniKeySize512";
+            this.mniKeySize512.Size = new System.Drawing.Size(152, 22);
+            this.mniKeySize512.Text = "512";
+            // 
+            // mniKeySize1024
+            // 
+            this.mniKeySize1024.Name = "mniKeySize1024";
+            this.mniKeySize1024.Size = new System.Drawing.Size(152, 22);
+            this.mniKeySize1024.Text = "1024";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.txtPrivateKey);
+            this.Controls.Add(this.txtPublicKey);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblKeySizeValue);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblKeySizeValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mniRSA;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtPublicKey;
+        private System.Windows.Forms.TextBox txtPrivateKey;
+        private System.Windows.Forms.ToolStripMenuItem mniKeySize;
+        private System.Windows.Forms.ToolStripMenuItem mniKeySize512;
+        private System.Windows.Forms.ToolStripMenuItem mniKeySize1024;
+    }
+}
+
