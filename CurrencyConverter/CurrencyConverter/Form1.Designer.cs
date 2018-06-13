29045276bd09f9f52e896bc3083c3ed94ed282e4
Choina <rafal.dziubak97@gmail.com>
2018-06-13 19:52:35 +0200

    Currency converter with current exchange rate(loading from NBP database)

---
+++ CurrencyConverter/CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD",
+            "GBP",
+            "EUR",
+            "PLN"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 10);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(289, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "USD",
+            "GBP",
+            "EUR",
+            "PLN"});
+            this.comboBox2.Location = new System.Drawing.Point(11, 62);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(289, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CONVERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(84, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 211);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CurrencyConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CurrencyConverter/CurrencyConverter/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml.XPath;
+using System.IO;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        List<float> list = new List<float>(); //List with currency exchange rates
+        float usd = new float();
+        float eur = new float();
+        float gbp = new float();
+        private void UpdatePr()
+        {
+            List <string> files = new List<string>();
+            WebClient ww = new WebClient();
+            ww.DownloadFile("http://www.nbp.pl/Kursy/xml/dir.txt", "dir.txt");
+
+            StreamReader rr = new StreamReader("dir.txt");
+            string line ="";
+            
+            while(line != null)
+            {
+                line = rr.ReadLine();
+
+                    if(line != null)
+                {
+                    if(line.StartsWith("a"))
+                    {
+                        files.Add(line);
+                    }
+                }
+
+            }
+            rr.Close();
+            XPathDocument document = new XPathDocument("http://www.nbp.pl/kursy/xml/" + files[files.Count - 1] + ".xml");
+            XPathNavigator navigator = document.CreateNavigator();
+            XPathNodeIterator iterator;
+            iterator = navigator.Select("tabela_kursow");
+
+            iterator = navigator.Select("tabela_kursow/pozycja");
+            foreach(XPathNavigator nav in iterator)
+            {
+                    if(nav.SelectSingleNode("kod_waluty").Value == "USD")
+                    {
+                    usd = float.Parse(nav.SelectSingleNode("kurs_sredni").Value);
+                    }
+                    if (nav.SelectSingleNode("kod_waluty").Value == "GBP")
+                    {
+                    gbp = float.Parse(nav.SelectSingleNode("kurs_sredni").Value);
+                }
+                    if (nav.SelectSingleNode("kod_waluty").Value == "EUR")
+                    {
+                    eur = float.Parse(nav.SelectSingleNode("kurs_sredni").Value);               }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            UpdatePr();
+        }
+        //Only numbers in textbox
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar)
+;        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Checking if fields are empty
+            if (this.comboBox1.SelectedItem == null || this.comboBox2.SelectedItem == null || String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please complete this information.");
+            }
+            else
+            {
+                float amount = float.Parse(textBox1.Text);
+                float rate = new float();
+                float rate2 = new float();
+                if (comboBox2.SelectedIndex == 0)
+                    rate = usd;
+                else if (comboBox2.SelectedIndex == 1)
+                    rate = gbp;
+                else if (comboBox2.SelectedIndex == 2)
+                    rate = eur;
+                else if (comboBox2.SelectedIndex == 3)
+                    rate = 1;
+
+                if (comboBox1.SelectedIndex == 0)
+                    rate2 = usd;
+                else if (comboBox1.SelectedIndex == 1)
+                    rate2 = gbp;
+                else if (comboBox1.SelectedIndex == 2)
+                    rate2 = eur;
+                else if (comboBox1.SelectedIndex == 3)
+                    rate2 = 1;
+                //PLN in combobox1
+                if (this.comboBox1.SelectedIndex == 3)
+                {
+                    label1.Text = (amount / rate).ToString();
+                }
+                else
+                {
+                    float pom = amount * rate2;
+                    label1.Text = (pom / rate).ToString();
+                }
+            }
+        }
+    }
+}
